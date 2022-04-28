1c69a5865c2d1502f851c4c342c8aa702558d9dd
NaiduNeha <95010096+NaiduNeha@users.noreply.github.com>
2022-04-28 08:41:00 +0530

    Create 6deciformat.py

---
+++ 6deciformat.py
@@ -0,0 +1,14 @@
+a=input("enter value of a:")
+b=input("enter value of b:")
+a=float(a)
+b=float(b)
+c=a/b
+print("{:.6f}".format(c))
+
+#OUTPUT:
+a=input("enter value of a:")
+b=input("enter value of b:")
+a=float(a)
+b=float(b)
+c=a/b
+print("{:.6f}".format(c))
